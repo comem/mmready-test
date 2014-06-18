4b6ab60963b89272c5c242705355924edd8cabf4
infinivis <romain.cerantola@heig-vd.ch>
2014-06-18 15:26:06 +0200

    Revert "InitialWithUnderscore&JQuery"
    This reverts commit 7633c37949e09a3b824f3dacf49d300da9d9c64d.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
 //inauguration de ce magnifique MAIN.JS!!!!!!!!!!!!!!
-
-var URLSERVEUR = "http://www.blabla.com" ;
-
-console.log('**********************');
-console.log('start coding..');
-console.log('**********************');
