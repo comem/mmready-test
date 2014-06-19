933e49b009ac04ef8e8ab4ec9009ef3ea0386a5d
umelec <fclerc@mbp-de-fabrice.einet.ad.eivd.ch>
2014-06-19 11:51:28 +0200

    ChangeST
    F. Change bacASable

--- js/main.js
+++
@@ -1,7 +0,0 @@
-//inauguration de ce magnifique MAIN.JS!!!!!!!!!!!!!!
-
-var URLSERVEUR = "http://www.blabla.com" ;
-
-console.log('**********************');
-console.log('start coding..');
-console.log('**********************');
