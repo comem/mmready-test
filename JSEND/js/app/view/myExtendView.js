d23b2ddfb302a8e01cb9f4d6880fc261f0a6cafe
NatyCana <naty@mbpdinacanonica.einet.ad.eivd.ch>
2014-06-20 15:58:47 +0200

    View

--- JSEND/js/app/view/myExtendView.js
+++ JSEND/js/app/view/myExtendView.js
@@ -2,38 +2,27 @@
 
 var MyView = Backbone.View.extend();
 
-var musicianView = MyView.extend({
-        initialize: function() {
-            this.listenTo(this.model, 'change', this.render);
-        },
-        render: function() {
-            console.log(this.model.attributes);
-            return this;
-        }
-    });
-    
-    
-    var artistView = MyView.extend({
-        initialize: function() {
-            this.listenTo(this.model, 'change', this.render);
-        },
-        render: function() {
-            console.log(this.model.attributes);
-            return this;
-        }
-    });
-    
-     var eventView = MyView.extend({
-        initialize: function() {
-            this.listenTo(this.model, 'change', this.render);
-        },
-        render: function() {
-            console.log(this.model.attributes);
-            return this;
-        }
-    });
-    
-    //////////////////////////////////////////////////////////////
-    
-    var musicien1 = new musicianModel();
-    var v1 = new musicianView({model: musicien1});
+var EventListView = MyView.extend({
+    template: _.template(templates.index),
+    initialize: function() {
+        this.listenTo(this.model, 'change add remove', this.render);
+        this.render();
+    }
+//    render: function() {
+//        console.log(this.model.attributes);
+//        return this;
+//    }
+});
+
+
+var ArtisteListView = MyView.extend({
+    template: _.template(templates.artistList),
+    initialize: function() {
+        this.listenTo(this.model, 'change add remove', this.render);
+        this.render();
+    }
+//    render: function() {
+//        console.log(this.model.attributes);
+//        return this;
+//    }
+});
