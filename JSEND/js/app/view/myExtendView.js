75efa644391d3d4b03b991b2e114808a39047198
NatyCana <naty@mbpdinacanonica.einet.ad.eivd.ch>
2014-06-20 16:11:56 +0200

    view

--- JSEND/js/app/view/myExtendView.js
+++ JSEND/js/app/view/myExtendView.js
@@ -3,7 +3,10 @@
 var MyView = Backbone.View.extend();
 
 var EventListView = MyView.extend({
-    template: _.template(templates.index),
+    template: _.template('../mmReadyHTMLCSS/index.html'),
+    events: {
+        "click #artist": "artist"
+    },
     initialize: function() {
         this.listenTo(this.model, 'change add remove', this.render);
         this.render();
@@ -14,15 +17,20 @@ var EventListView = MyView.extend({
 //    }
 });
 
-
-var ArtisteListView = MyView.extend({
-    template: _.template(templates.artistList),
-    initialize: function() {
-        this.listenTo(this.model, 'change add remove', this.render);
-        this.render();
-    }
-//    render: function() {
-//        console.log(this.model.attributes);
-//        return this;
+//
+//var ArtistListView = MyView.extend({
+//    template: _.template('../mmReadyHTMLCSS/artistList.html'),
+//    initialize: function() {
+//        this.listenTo(this.model, 'change add remove', this.render);
+//        this.render();
 //    }
-});
+////    render: function() {
+////        console.log(this.model.attributes);
+////        return this;
+////    }
+//});
+
+
+var artist = function() {
+    console.log('artist');
+};
