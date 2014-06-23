74a55700d9ff06f6a77e20a2f1f430ab2049da2c
NatyCana <naty@mbpdinacanonica.einet.ad.eivd.ch>
2014-06-23 11:34:29 +0200

    modif js

--- JSEND/services/EventCollectionSUCCESS.php
+++ JSEND/services/EventCollectionSUCCESS.php
@@ -2,24 +2,20 @@
 
 echo '{
 status : "success",
- data : [{"id":"1", "name": "Event1", "Artists":
-[
-{"id" : "1", "name" : "MMReady()", "Musicians" : [
-{ "id" : "1", "name" : "Clélia", "Instruments" : "Vocal" },
- { "id" : "2", "name" : "Fabrizio", "Instruments" : "Guitar" },
- { "id" : "3", "name" : "Naty", "Instruments" : "Synth" },
- { "id" : "4", "name" : "Romain", "Instruments" : "Bass" },
- { "id" : "5", "name" : "Maya", "Instruments" : "Drums" }
-]},
- {"id" : "2", "name" : "ZED", "Musicians" : [
-{ "id" : "6", "name" : "Richard", "Instruments" : "Vocal" },
- { "id" : "7", "name" : "Adam", "Instruments" : "Guitar" },
- { "id" : "8", "name" : "Matthieu", "Instruments" : "Drums" },
- { "id" : "9", "name" : "Laurent", "Instruments" : "Bass" }
-]}
-]
-},{"id":"1", "name": "Event2"}]
-}';
+ data : [{"title":"La grosse fiesta 2014",
+          "artists":
+                     {"name":"mmready()",
+                     "musicians":
+                                  {"name":"Romain","instruments":[{"name":"Bass"}]},
+                                  {"name":"Clélia","instruments":[{"name":"Vocal"}]}
+                                 ]},
+                     
+
+                     {"name":"ZED","musicians":[]}
+                     ]
+        }]
+        
+       }';
 
 
     
