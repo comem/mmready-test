3948ba83d29409e02620c852b88d4d32e8fe16f3
NatyCana <naty@mbpdinacanonica.einet.ad.eivd.ch>
2014-06-24 16:15:17 +0200

    Structure dossier MVC

---
+++ MVC/js/variables.js
@@ -0,0 +1,6 @@
+/////////////////////// VARIABLES GLOBALES //////////////////////////////
+// /!\ url depuis index.html
+    var URLSERVEURsuccess = "services/EventCollectionSUCCESS.php";
+    var URLSERVEURfail = "services/EventCollectionFAIL.php";
+    var URLSERVEURerror = "services/EventCollectionERROR.php";
+    var ONLINE;
