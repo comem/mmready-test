54fa3993938d6fc60f7a04df032b214868d5b5cb
infinivis <romain.cerantola@heig-vd.ch>
2014-06-26 01:06:08 +0200

    dispatchFile

--- MVC/js/model/myExtendModel.js
+++ MVC/js/model/myExtendModel.js
@@ -1,3 +1,43 @@
 //***************************************************************************//
-//******************* Extension Model                 ***********************//
+//*******************           Model                 ***********************//
 //***************************************************************************//
+
+
+var MyModel = Backbone.Model.extend();
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+var MyModelNestedCollection = MyModel.extend({
+    nested: 'collection',
+    initialize: function (attrs, options) { // universalisation des noms de la variable contenant la collection  
+        
+        console.log(attrs[this.nested].toJSON());
+        console.log(JSON.stringify(attrs[this.nested]));
+        
+        if (typeof attrs[this.nested] !== "undefined") { //SI le type de l'attribut
+                                                        // de ce nom n'est pas indéfini '
+            this.set(this.nested, new window[this.nested.capitalize()](attrs[this.nested])); // ALORS crée une instance portant
+                                                                                             // le nom en lui ajoutant
+                                                                                             // une majuscule sur le premier
+                                                                                             // caractère
+        }else{
+             attrs[this.nested] = [];
+        }
+//        this.get(this.nested).on('all', function(eventName) {
+//            this.trigger(eventName, this);
+//        }, this);
+    },
+    toJSON: function(attrs, options) {//surcharge de la fonction JSON pour stringifyer une collection contenue dans un attribut d'un model
+        if(this.nested === 'undefined'){
+            console.log('this.nested ( '+this.name+' ) is undefined');
+            return;
+        }
+        var colObj = {};
+        colObj[this.nested] = this.get(this.nested).toJSON();
+        return _.extend(
+            Backbone.Model.prototype.toJSON.apply(this, arguments),
+            colObj
+        );
+    }
+});
