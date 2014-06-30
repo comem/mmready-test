a25ab62e04f2f047e98d45e2f088eab6900cee3d
may0u <maya.jeanmonod@gmail.com>
2014-06-30 16:49:56 +0200

    Revert "add event + js quasi fini?"
    This reverts commit 88fd8e1542b720966a8cb8170dab78b34721c035.

--- MMReadyDesign/js/maya.js
+++ MMReadyDesign/js/maya.js
@@ -1,35 +1,20 @@
-var templateTicket;
-var templateInstrument;
-var templateMusician;
-var templateLink;
-
 $(document).ready(function() {
-    templateMusician = $('.musician').clone();
-    $('#musicians').empty();
-    templateTicket = $(".ticket").clone();
-    templateInstrument = $(".instrument").clone();
-    templateImage = $('.image').clone();
-    $('#images').empty();
-    templateLink = $('.link').clone();
-    $('#links').empty();
     $('#addTicketCategory').on('click', addTicket);
     $('#addMusician').click(addMusician);
-    //$('#addInstrument').click(addInstrument);
-    $('#addImage').click(addImage);
-    $('#addLink').click(addLink);
+    $('#addInstrument').click(addInstrument);
+
 });
 
 ////***********************************************************************************
 ////*************************************TICKETS***************************************
 ////***********************************************************************************
 function addTicket() {
-    var ticket = templateTicket;
     //récupérer le nombre de ticket actuellement visibles
     var nbTickets = $(".ticket").length;
     //incrémenter l'id du prochain ticket
     var nextId = nbTickets + 1;
     //cloner et rajouter au dom
-    $(ticket).clone().appendTo("#selectTicket");
+    $(".ticket").first().clone().appendTo("#selectTicket");
     //modifier l'id du dernier élément ajouté
     $(".ticket").last().attr('id', 'ticket_' + nextId);
     //faire apparaître
@@ -53,16 +38,14 @@ function addMusician() {
     //incrémenter l'id du prochain ticket
     var nextId = nbMusicians + 1;
     //cloner et rajouter au dom
-    var musician = templateMusician.clone();
-    $(musician).appendTo("#musicians");
+    $(".musician").first().clone().appendTo("#musicians");
     //modifier l'id du dernier élément ajouté
     $(".musician").last().attr('id', 'musician_' + nextId);
     //faire apparaître
     $(".musician").last().removeClass('hide');
     //supprimer un musicien
     $('.deleteMusician').click(deleteMusician);
-    //ajout d'instruments
-    $('.addInstrument').click(addInstrument);
+
 }
 
 function deleteMusician() {
@@ -72,71 +55,22 @@ function deleteMusician() {
 //***********************************************************************************
 //*************************************INSTRUMENt************************************
 //***********************************************************************************
-//function addInstrument() {
-//    var instrument = templateInstrument.clone();
-//    //récupérer le nombre de ticket actuellement visibles
-//    var nbInstruments = $(".instrument").length;
-//    //incrémenter l'id du prochain ticket
-//    var nextId = nbInstruments + 1;
-//
-//    //cloner et rajouter au dom
-//    $(instrument).clone().appendTo(".instruments");
-//    //modifier l'id du dernier élément ajouté
-//    $(".instrument").last().attr('id', 'instrument_' + nextId);
-//    //faire apparaître
-//    $(".instrument").last().removeClass('hide');
-//
-//    //ajouter le bouton supprimer
-//    $('<button class="pull-right deleteInstrument"><i class="ico-delete"></i>Delete</button>').appendTo($(".instrument").last());
-//    //supprimer un instrument
-//    $('.deleteInstrument').on('click', deleteInstrument);
-//}
-//
-//function deleteInstrument() {
-//    $(this).parent().remove();
-//}
-
-//***********************************************************************************
-//*************************************IMAGES**************************************
-//***********************************************************************************
-function addImage() {
+function addInstrument() {
     //récupérer le nombre de ticket actuellement visibles
-    var nbImage = $(".image").length;
+    var nbInstruments = $(".instrument").length;
     //incrémenter l'id du prochain ticket
-    var nextId = nbImage + 1;
+    var nextId = nbInstruments + 1;
     //cloner et rajouter au dom
-    var image = templateImage.clone();
-    $(image).appendTo("#images");
+    $(".instrument").first().clone().appendTo("#instruments");
     //modifier l'id du dernier élément ajouté
-    $(".image").last().attr('id', 'image_' + nextId);
+    $(".instrument").last().attr('id', 'instrument_' + nextId);
     //faire apparaître
-    $(".image").last().removeClass('hide');
+    $(".instrument").last().removeClass('hide');
     //supprimer un musicien
-    $('.deleteImage').click(deleteMusician);
+    $('.deleteInstrument').click(deleteInstrument);
+
 }
 
-function deleteMusician() {
-    $(this).parent().remove();
-}
-//***********************************************************************************
-//*************************************LINKS*****************************************
-//***********************************************************************************
-function addLink() {
-    //récupérer le nombre de ticket actuellement visibles
-    var nbLink = $(".link").length;
-    //incrémenter l'id du prochain lien
-    var nextId = nbLink + 1;
-    //cloner et rajouter au dom
-    var image = templateLink.clone();
-    $(image).appendTo("#links");
-    //modifier l'id du dernier élément ajouté
-    $(".link").last().attr('id', 'link_' + nextId);
-    //faire apparaître
-    $(".link").last().removeClass('hide');
-    //supprimer un lien
-    $('.deleteLink').click(deleteMusician);
-}
-
-function deleteMusician() {
+function deleteInstrument() {
     $(this).parent().remove();
 }
